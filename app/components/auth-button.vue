403316619dd965728ed99007ed3d9beefad95523
silky <jarrod.silk.primary@gmail.com>
2025-10-12 18:44:40 +1000

    add github login

---
+++ app/components/auth-button.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { useAuthStore } from "~/stores/auth";
+
+const authStore = useAuthStore();
+const { isLoading, signIn } = authStore;
+</script>
+
+<template>
+  <button :disabled="isLoading" class="btn btn-accent" @click="signIn">
+    Sign In With Github
+    <span v-if="isLoading" class="loading loading-spinner loading-md" />
+    <Icon v-else name="tabler:brand-github" size="24" />
+  </button>
+</template>

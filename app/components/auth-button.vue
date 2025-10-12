2e9677eb8e86726e49bcaa7dd8a63cb2f02c5861
silky <jarrod.silk.primary@gmail.com>
2025-10-12 19:37:54 +1000

    add auth state and sign out

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -1,16 +1,31 @@
 <script lang="ts" setup>
-import { storeToRefs } from "pinia";
-
 import { useAuthStore } from "~/stores/auth";
 
 const authStore = useAuthStore();
-const { isLoading } = storeToRefs(authStore);
 </script>
 
 <template>
-  <button :disabled="isLoading" class="btn btn-accent" @click="authStore.signIn">
+  <div v-if="!authStore.isLoading && authStore.user" class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn m-1">
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </div>
+    <ul tabindex="-1" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+  <button v-else :disabled="authStore.isLoading" class="btn btn-accent" @click="authStore.signIn">
     Sign In With Github
-    <span v-if="isLoading" class="loading loading-spinner loading-md" />
+    <span v-if="authStore.isLoading" class="loading loading-spinner loading-md" />
     <Icon v-else name="tabler:brand-github" size="24" />
   </button>
 </template>
